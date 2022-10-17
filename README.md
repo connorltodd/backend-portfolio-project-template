fe1542a9d3297b8a90fb0d6ce33006398929ecce
Connor Todd <connortodd0304@gmail.com>
2022-10-17 11:33:02 +0100

    initial template setup implemented minus the readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+Write here the setup to get the project running
+
+Add in link to the powerpoint
+
+
+.env file 
+DB_HOST='localhost'
+DB_USER='root'
+DB_PASSWORD='your_current_password'
+DATABASE='fake_store_db_portfolio_project'
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan')
+const cors = require('cors');
+const app = express();
+const PORT = 9000;
+const { connection } = require('./config')
+
+const productsRouter = require('./routes/products.route');
+const usersRouter = require('./routes/user.route');
+const cartRouter = require('./routes/cart.route');
+
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(cors("*"));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use('/products', productsRouter);
+app.use('/users', usersRouter);
+app.use('/cart', cartRouter);
+
+
+connection.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('Successfully connected to database')
+    }
+})
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log('error: ', error)
+    } else {
+        console.log(`App is running at ${PORT}`)
+    }
+});
